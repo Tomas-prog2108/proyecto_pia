173f87cbec172d099c76e8e3b9faf4f50be5aa05
Tomas2108 <tomasmartineztangarife@gmail.com>
2026-06-18 13:37:29 -0500

    L

--- static/js/proyectos_dis.js
+++ static/js/proyectos_dis.js
@@ -1,3 +1,17 @@
 setTimeout(function() {
   document.querySelector('.main').classList.add('visible');
 }, 100);
+
+const botones = document.querySelectorAll(".btn-ins");
+
+botones.forEach(boton => {
+  boton.addEventListener("click", () => {
+    const snackbar = document.getElementById("snackbar");
+
+    snackbar.classList.add("show");
+
+    setTimeout(() => {
+      snackbar.classList.remove("show");
+    }, 3000);
+  });
+});
